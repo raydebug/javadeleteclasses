17047b60fd3622918e7a6f73896bbae240f8d18a
Ray <raydebug@gmail.com>
2024-12-13 15:11:33 +1000

    fix

--- src/main/java/com/example/test/TestDependency.java
+++ src/main/java/com/example/test/TestDependency.java
@@ -1,6 +1,11 @@
 package com.example.test;
 
 public class TestDependency {
+    public TestSubDependency getSubDependency() {
+        return subDependency;
+    }
+
+    private TestSubDependency subDependency= new TestSubDependency();
     public void doSomething() {
         System.out.println("Test");
     }
